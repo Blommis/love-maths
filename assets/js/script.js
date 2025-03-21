65d08dee94d0fe19ddd299a43f558ada7e507900
Blommis <blomquist799@gmail.com>
2025-03-21 10:38:06 +0100

    set up structure for javascript

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongScore(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
+
+
